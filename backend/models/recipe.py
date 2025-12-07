96e846000ac86d907c6706497d650c33412b52bb
Chrismiles <mileschris466@gmail.com>
2025-12-08 00:11:19 +0300

    fixing cors in the backend

--- backend/models/recipe.py
+++ backend/models/recipe.py
@@ -1,13 +1,17 @@
 from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from database import Base
 
-
 class Recipe(Base):
     __tablename__ = "recipes"
 
     recipe_id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.user_id"))
-    title = Column(String)
-    description = Column(Text)
-    ingredients = Column(Text)
-    instructions = Column(Text)
+
+    title = Column(String, nullable=False)
+    ingredients = Column(Text, nullable=False)
+    instructions = Column(Text, nullable=False)
+
+    category = Column(String, nullable=True)
+    prep_time = Column(Integer, nullable=True)
+    image_url = Column(String, nullable=True)
+
